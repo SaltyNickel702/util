4eca4d4f5ab0e594d8624e5620a5e3b93c276ca3
DarkMatr126 <95891701+DarkMatr126@users.noreply.github.com>
2022-10-03 16:28:51 -0600

    Update js_import.js

--- js/js_import.js
+++ js/js_import.js
@@ -2,9 +2,17 @@ if (typeof js === 'undefined') {
     js = {};
 }
 js.import = (fileUrl) => {
-    const head = document.head;
-    const link = document.createElement('script');
-    link.setAttribute('src',fileUrl);
-
-    head.append(link);
+    var importerAdded = false;
+    for (let i = 0; i < document.getElementsByTagName('script').length; i++) {
+        var doc = document.getElementsByTagName('script')[i];
+        if (doc.getAttribute('src') === fileUrl) {
+            importerAdded = true;
+        }
+    }
+    if (!importerAdded) {
+        var head = document.head;
+        var link = document.createElement('script');
+        link.setAttribute('src',fileUrl);
+        head.append(link);
+    }
 }
